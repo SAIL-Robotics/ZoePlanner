26b2453734a38d77620da48760fa961af91fe344
preethibaskar <preethibaskar90@gmail.com>
2014-12-20 00:09:21 +0530

    execution date changes

--- RoverTool/views.py
+++ RoverTool/views.py
@@ -53,9 +53,12 @@ def DBOperation(request):
             if request.POST['operation'] == 'save':
                 plan_name = request.POST['planName']
                 plan_desc = request.POST['planDesc']
+                plan_date = request.POST['executionDate']
+                print '**********'
+                print plan_date
                 plan_name_update = request.POST['planNameUpdate']
                 markers = json.loads(request.POST.get('markers'))
-                saveToDB(plan_name, plan_name_update, plan_desc, markers)
+                saveToDB(plan_name, plan_name_update, plan_desc, markers,plan_date)
                 data = get_plan_detail()                                    #update the let pane. getting plan names from 
             elif request.POST['operation'] == 'getMarkerInfo':
                 plan_name = request.POST['planName']
@@ -134,7 +137,7 @@ def DBOperation(request):
     return HttpResponse(json.dumps(data), content_type = "application/json")
 
 
-def saveToDB(plan_name, plan_name_update, plan_desc, markers):    
+def saveToDB(plan_name, plan_name_update, plan_desc, markers,plan_date):    
     connection = Connection()
 
     now = datetime.datetime.now()
@@ -144,7 +147,7 @@ def saveToDB(plan_name, plan_name_update, plan_desc, markers):
     collection = db[collection_name]
     
     if validatePlanName(plan_name_update)['count'] == 0:
-        plan = {"planName" : plan_name, "planDescription" : plan_desc, "timeStamp" : time, "markers" : markers}
+        plan = {"planName" : plan_name, "planDescription" : plan_desc, "timeStamp" : time, "markers" : markers, "planDate" : plan_date}
         collection.save(plan)
     else:
         print "update"
